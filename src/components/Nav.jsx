4235240b4986471e913c63ee8272c09af6bb7d32
Sami-20s <samikfouri22@gmail.com>
2021-09-24 20:34:48 +0300

    nav added

---
+++ src/components/Nav.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Logo } from "./svg.jsx";
+const liList = [
+    { id: 1, title: "About Me" },
+    { id: 2, title: "Services" },
+    { id: 3, title: "Portfolio" },
+    { id: 4, title: "Testimonials" },
+    { id: 5, title: "Blog" },
+    { id: 6, title: "Contact Us" },
+];
+const Nav = () => {
+    return (
+        <nav className="container">
+            <div className="nav__wrapper ">
+                <Logo className="logo" />
+                <div className="nav__wrapper__navbar">
+                    <ul>
+                        {liList.map(el => {
+                            const { id, title } = el;
+                            return (
+                                <li key={id}>
+                                    <a href={`#${title.toLocaleLowerCase().split(" ")[0]}`}>
+                                        {title}
+                                    </a>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    <button className="btn">Hire Me!</button>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
---
+++ src/components/svg.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Logo = ({ className }) => {
+    return (
+        <>
+            <img src="./img/logo.svg" alt="" className={className} />
+        </>
+    );
+};
